e51b4302f55e9e7f95c4e2b6d41923ccd95f7ea2
Dan Podeanu <dapodean@microsoft.com>
2026-08-05 20:43:38 +0800

    Release firmware 1.7.1

--- common/include/version.h
+++ common/include/version.h
@@ -18,7 +18,7 @@
 // preprocessor macro (for string concatenation into rodata markers),
 // matching the pattern used for MODEL_NAME / MODEL_NAME_LITERAL.
 
-#define FIRMWARE_VERSION_LITERAL "1.7.0"
+#define FIRMWARE_VERSION_LITERAL "1.7.1"
 
 namespace board {
 
